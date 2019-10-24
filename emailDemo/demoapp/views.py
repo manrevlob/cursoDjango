d5d5c8d8b466f4ff653ed0140668f4ca3c01c18c
Manuel Juan Revilla Lobo <manuel.revillalobo@plexus.local>
2019-10-24 16:59:15 +0200

    Subida inicial del curso Django
    En esta subida se harán los commits de las secciones correspondientes:
    * Sección 1: Introducción
    * Sessión 2: Database Driven Dynamic Website
    * Sección 3: Django App
    * Sección 4: Messaging And Email Form

---
+++ emailDemo/demoapp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+
+from demo.settings import EMAIL_HOST_USER
+from demoapp.forms import ContactForm
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {'page': 'home'})
+
+def about(request):
+    return render(request, 'about.html', {'page': 'about'})
+
+def send_mail(contact_form_data):
+    email_message_format = 'name: %s\nemail: %s\nMessage: %s\n'
+    name = contact_form_data.get('name', '')
+    message = contact_form_data.get('message', '')
+    email = contact_form_data.get('email')
+    email_message_format = email_message_format % (name, email_message)
+    send_mail('Demo Website', email_message_format, EMAIL_HOST_USER, [EMAIL_HOST_USER]), fail_silently=false,)
+
+def contact_us(request):
+    try:
+        if request.method == 'POST':
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                send_mail(form.cleaned_data)
+                messages.success(request, 'El mensaje ha sido enviado.')
+        else:
+            form = ContactForm()
+    except:
+        messages.error(request, 'contact.html', {'page':'contact', 'form':form})
+
+def clear(request):
+    form = ContactForm()
+    messages.error(request, 'Campos borrados satisfactoriamente.')
+    return render(request, 'contact.html', {'page':'contact', 'form':form})
