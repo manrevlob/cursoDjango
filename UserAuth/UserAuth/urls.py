09d9087e6c4ab149ae6a08f7b830777f47440081
Manuel Revilla Lobo <manuelrl@ext.inditex.com>
2020-03-19 12:23:54 +0100

    Subida del proyecto de autenticación

---
+++ UserAuth/UserAuth/urls.py
@@ -0,0 +1,27 @@
+"""UserAuth URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from user_auth.views import index, signup, login_view, change_password, logout_user
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', signup, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('change_password/', change_password, name='change_password'),
+    path('', index, name='index')
+]
